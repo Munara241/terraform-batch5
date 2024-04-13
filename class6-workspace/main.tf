3edb5721c9cd480268e5d2fe331a2efe387747e9
munara <munara.24185@gmail.com>
2024-04-13 17:02:51 +0000

    pushh

---
+++ class6-workspace/main.tf
@@ -0,0 +1,15 @@
+
+provider aws {
+    region = var.region
+}
+
+variable region {
+  type        = string
+  default     = ""
+}
+
+resource "aws_key_pair" "deployer" {
+  key_name   = "kaizen-key"
+  public_key = file("~/.ssh/id_rsa.pub")
+  }
+
